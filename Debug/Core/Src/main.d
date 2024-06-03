88e4c441968f408bd7c311114d668231e1e13ed5
Memeslav <122861199+Memeslav@users.noreply.github.com>
2024-06-03 10:47:17 +0300

    Updated dependencies

---
+++ Debug/Core/Src/main.d
@@ -0,0 +1,17 @@
+Core/Src/main.o: ../Core/Src/main.c \
+ C:/Users/vv.stepanishin/Desktop/Current-Measurement-Module/Core/Inc/main.h \
+ C:/Users/vv.stepanishin/Desktop/Current-Measurement-Module/CMSIS/Inc/stm32l053xx.h \
+ C:/Users/vv.stepanishin/Desktop/Current-Measurement-Module/CMSIS/Inc/core_cm0plus.h \
+ C:/Users/vv.stepanishin/Desktop/Current-Measurement-Module/CMSIS/Inc/cmsis_version.h \
+ C:/Users/vv.stepanishin/Desktop/Current-Measurement-Module/CMSIS/Inc/cmsis_compiler.h \
+ C:/Users/vv.stepanishin/Desktop/Current-Measurement-Module/CMSIS/Inc/cmsis_gcc.h \
+ C:/Users/vv.stepanishin/Desktop/Current-Measurement-Module/CMSIS/Inc/mpu_armv7.h \
+ C:/Users/vv.stepanishin/Desktop/Current-Measurement-Module/CMSIS/Inc/system_stm32l0xx.h
+C:/Users/vv.stepanishin/Desktop/Current-Measurement-Module/Core/Inc/main.h:
+C:/Users/vv.stepanishin/Desktop/Current-Measurement-Module/CMSIS/Inc/stm32l053xx.h:
+C:/Users/vv.stepanishin/Desktop/Current-Measurement-Module/CMSIS/Inc/core_cm0plus.h:
+C:/Users/vv.stepanishin/Desktop/Current-Measurement-Module/CMSIS/Inc/cmsis_version.h:
+C:/Users/vv.stepanishin/Desktop/Current-Measurement-Module/CMSIS/Inc/cmsis_compiler.h:
+C:/Users/vv.stepanishin/Desktop/Current-Measurement-Module/CMSIS/Inc/cmsis_gcc.h:
+C:/Users/vv.stepanishin/Desktop/Current-Measurement-Module/CMSIS/Inc/mpu_armv7.h:
+C:/Users/vv.stepanishin/Desktop/Current-Measurement-Module/CMSIS/Inc/system_stm32l0xx.h:
