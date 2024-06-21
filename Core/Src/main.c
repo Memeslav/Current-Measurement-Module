81f7a452a901c44899fbe6f55363fbc03d1779cd
skugas <kugachv@gmail.com>
2024-06-21 15:18:01 +0300

    Delete strins in main

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -5,12 +5,6 @@ int main(void)
 {
     RCC->IOPENR |= RCC_IOPENR_GPIOAEN;
 
-        GPIOA->MODER &= ~(GPIO_MODER_MODE9_Msk);
-        GPIOA->MODER |=  (GPIO_MODER_MODE9_1);
-
-        RCC->CFGR &= ~(RCC_CFGR_MCOSEL_Msk | RCC_CFGR_MCOPRE_Msk);
-        RCC->CFGR |= RCC_CFGR_MCOSEL_0 | RCC_CFGR_MCOSEL_1;
-
     RCC->ICSCR &= ~RCC_ICSCR_MSIRANGE_Msk;
 
     RTC_Enable();
