902e0dda81e7425b64d16be4bfed619365187974
Memeslav <122861199+Memeslav@users.noreply.github.com>
2024-10-29 15:24:08 +0300

    WORKED STOP MODE
    Рабочий код ввода ядра в режим STOP!!!

--- Core/Src/PKM.c
+++ Core/Src/PKM.c
@@ -1,115 +1,105 @@
 #include "PKM.h"
+#include <string.h>
 
+#define PKM_BUFFER_SIZE  32U
+#define UPDATE_DELAY     30U
 
-#define 	PKM_BUFFER_SIZE		32U
-#define 	UPDATE_DELAY		30U
+typedef struct {
+    uint16_t data[PKM_BUFFER_SIZE];
+    uint16_t index;
+    uint16_t command;
+    uint16_t start_address;
+    uint16_t register_count;
+} SPI_BUFFER;
 
-typedef struct
-{
-	uint16_t data[PKM_BUFFER_SIZE];
+static SPI_BUFFER buffer = {{0}, 0, 0, 0, 0};
 
-	uint16_t index;
-	uint16_t command;
-	uint16_t start_address;
-	uint16_t register_count;
+// Включаем необходимые периферийные модули
+void PKM_Enable(void) {
+    CRC_Enable();
+    SPI1_Enable();
+};
+
+// Сброс и очистка буфера при прерывании на внешней линии
+void EXTI4_15_IRQHandler(void) {
+    if (EXTI->PR & EXTI_PR_PIF4) {
+        if (!(GPIOA->IDR & GPIO_IDR_ID4)) {
+            memset(&buffer, 0, sizeof(buffer));
+            CRC->CR |= CRC_CR_RESET; // Сброс модуля CRC
+        }
+        EXTI->PR |= EXTI_PR_PIF4;
+    }
+    NVIC_ClearPendingIRQ(EXTI4_15_IRQn);
 }
-SPI_BUFFER;
-SPI_BUFFER buffer = {{0}, 0, 0, 0, 0};
 
-void PKM_Enable(void)
-{
-	CRC_Enable();
-	SPI1_Enable();
+// Извлечение команды и начального адреса из первого элемента буфера данных
+static inline void ParseCommandAndAddress(void) {
+    buffer.command       = buffer.data[0] & 0x8000;
+    buffer.start_address = buffer.data[0] & 0x7FFF;
 }
 
-void EXTI4_15_IRQHandler(void)
-{
-	if(EXTI->PR & EXTI_PR_PIF4)
-	{
-		if(!(GPIOA->IDR & GPIO_IDR_ID4))
-		{
-			GPIOA->MODER |=	 GPIO_MODER_MODE6_Msk;
-			GPIOA->MODER &=	~GPIO_MODER_MODE6_0;
-
-			memset(&buffer, 0, sizeof(buffer));
-			CRC->CR |= CRC_CR_RESET;
-		}
-		else
-		{
-			GPIOA->MODER |=	 GPIO_MODER_MODE6_Msk;
-			GPIOA->MODER &=	~GPIO_MODER_MODE6_1;
-			GPIOA->BSRR |= GPIO_BSRR_BR_6;
-		}
-
-		EXTI->PR |= EXTI_PR_PIF4;
-	}
-	NVIC_ClearPendingIRQ(EXTI4_15_IRQn);
+// Извлечение количества регистров из второго элемента буфера данных
+static inline void ParseRegisterCount(void) {
+    buffer.register_count = __REV16(buffer.data[1]);
 }
-void SPI1_IRQHandler(void)
-{
-	if(SPI1->SR & SPI_SR_RXNE)
-	{
-		buffer.data[buffer.index] = SPI1->DR;
 
-		if(buffer.index == 0)
-		{
-			buffer.command 			= buffer.data[0] & 0x8000;
-			buffer.start_address 	= buffer.data[0] & 0x7FFF;
-		}
+// Запись данных из буфера в регистры, если CRC совпадает
+static void WriteDataFromPKM(void) {
+    if (buffer.index != (2 + buffer.register_count)) return;
 
-		if(buffer.index == 1)
-		{
-			buffer.register_count  = __REV16(buffer.data[1]);
-		}
+    // Проверка CRC данных
+    for (uint8_t i = 0; i < buffer.index; i++) {
+        *((__IO uint16_t *)&CRC->DR) = buffer.data[i];
+    }
 
-		if(buffer.command)
-		{
-			if(buffer.index == (2 + buffer.register_count))
-			{
-				for(uint8_t i = 0; i < buffer.index; i++)
-				{
-					*((__IO uint16_t *)&CRC->DR) = buffer.data[i];
-				}
+    if (__REV16(CRC->DR) == buffer.data[buffer.index]) {
+        // Обновление регистров
+        for (uint8_t i = 0; i < buffer.register_count; i++) {
+            regs[buffer.start_address + i] = __REV16(buffer.data[2 + i]);
+        }
+    }
+}
 
-				if(__REV16(CRC->DR) == buffer.data[buffer.index])
-				{
-					uint8_t increment = 0;
+// Загрузка данных в SPI с CRC-контролем
+static void LoadDataToPKM(void) {
+    switch (buffer.index) {
+        case 0:
+            *((__IO uint16_t *)&CRC->DR) = buffer.start_address;
+            SPI1->DR = buffer.start_address;
+            break;
+        case 1:
+            *((__IO uint16_t *)&CRC->DR) = __REV16(buffer.register_count);
+            SPI1->DR = __REV16(buffer.register_count);
+            break;
+        default:
+            if (buffer.index < (2 + buffer.register_count)) {
+                *((__IO uint16_t *)&CRC->DR) = __REV16(regs[buffer.start_address]);
+                SPI1->DR = __REV16(regs[buffer.start_address]);
+                buffer.start_address++;
+            } else if (buffer.index == (2 + buffer.register_count)) {
+                SPI1->DR = __REV16(CRC->DR);
+            }
+            break;
+    }
+}
 
-					for(uint8_t i = buffer.start_address; i < (buffer.start_address + buffer.register_count); i++)
-					{
-						regs[i] = __REV16(buffer.data[2 + increment]);
+// Обработчик прерывания SPI1
+void SPI1_IRQHandler(void) {
+    if (SPI1->SR & SPI_SR_RXNE) {
+        buffer.data[buffer.index] = SPI1->DR;
 
-						increment++;
-					}
-				}
-			}
-		}
-		else
-		{
-			if		(buffer.index == 0)
-	        {
-				*((__IO uint16_t *)&CRC->DR) = buffer.start_address;
-									SPI1->DR = buffer.start_address;
-	        }
-			else if	(buffer.index == 1)
-	        {
-				*((__IO uint16_t *)&CRC->DR) = __REV16(buffer.register_count);
-									SPI1->DR = __REV16(buffer.register_count);
-	        }
-			else if	(buffer.index <  (2 + buffer.register_count))
-	        {
-				*((__IO uint16_t *)&CRC->DR) = __REV16(regs[buffer.start_address]);
-									SPI1->DR = __REV16(regs[buffer.start_address]);
-									buffer.start_address++;
-	        }
-			else if (buffer.index == (2 + buffer.register_count))
-	        {
-				SPI1->DR = __REV16(CRC->DR);
-	        }
-		}
+        // Парсинг команды и регистра только на первых шагах
+        if (buffer.index == 0) ParseCommandAndAddress();
+        if (buffer.index == 1) ParseRegisterCount();
 
-		buffer.index++;
-	}
+        // Выбор операции в зависимости от команды
+        if (buffer.command) {
+            WriteDataFromPKM();
+        } else {
+            LoadDataToPKM();
+        }
 
-	NVIC_ClearPendingIRQ(SPI1_IRQn);
+        buffer.index++;
+    }
+    NVIC_ClearPendingIRQ(SPI1_IRQn);
 }
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,11 +1,115 @@
 #include "main.h"
 
+#define     PREDIV_A_Default    0x7F
+#define 	PREDIV_S_Default	0xFF
+//Ключи для доступа к RTC
+#define 	RTC_KEY_0 			0xCA
+#define 	RTC_KEY_1 			0x53
+#define 	RTC_KEY_OFF			0xFF
+//Параметры для WakeUp Timer
+#define 	WAKE_UP_DIV			32U
+#define 	WAKE_UP_MS_TO_SEC	1000U
+
+float RTC_Frequence = 32768;
+
+uint8_t meme = 0;
+
+void Real_time_clock_enable(void)
+{
+    RCC->APB1ENR |=  RCC_APB1ENR_PWREN;
+
+		PWR->CR  |=  PWR_CR_DBP;
+
+		RCC->CSR |=  RCC_CSR_RTCRST_Msk;
+		RCC->CSR &= ~RCC_CSR_RTCRST_Msk;
+
+		RCC->CSR |=	 RCC_CSR_LSEON;
+		while((RCC->CSR & RCC_CSR_LSERDY) != RCC_CSR_LSERDY){}
+
+		RCC->CSR |=  RCC_CSR_RTCSEL_0;
+		RCC->CSR |=  RCC_CSR_RTCEN;
+
+		PWR->CR  &= ~PWR_CR_DBP;
+
+	RCC->APB1ENR &= ~RCC_APB1ENR_PWREN;
+}
+
+void RTC_Set_WakeUp_Timer(void)
+{
+	RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+
+		PWR->CR |= PWR_CR_DBP;
+
+		RTC->WPR  =  RTC_KEY_0;
+		RTC->WPR  =  RTC_KEY_1;
+
+		RTC->CR  &= ~RTC_CR_WUTE;
+		RTC->CR  &= ~RTC_CR_WUCKSEL_Msk;
+		RTC->CR  |=  RTC_CR_WUCKSEL_2;
+		RTC->ISR &= ~RTC_ISR_WUTF;
+
+		RTC->WUTR = 2;
+
+		RTC->CR |=  RTC_CR_WUTE |
+					RTC_CR_WUTIE;
+
+		EXTI->RTSR |= EXTI_RTSR_RT20;
+		EXTI->IMR  |= EXTI_IMR_IM20;
+
+		RTC->WPR  = 0x00;
+
+		RCC->APB1ENR &= ~RCC_APB1ENR_PWREN;
+
+	NVIC_SetPriority(RTC_IRQn, 2);
+	NVIC_EnableIRQ(RTC_IRQn);
+}
+
+void RTC_IRQHandler(void)
+{
+	if (RTC->ISR & RTC_ISR_WUTF)
+	{
+		RTC->ISR &= ~RTC_ISR_WUTF;
+		EXTI->PR =   EXTI_PR_PIF20;
+
+		if(meme%2 == 0)
+		{
+			RCC->IOPENR  |=   RCC_IOPENR_GPIOAEN;
+			GPIOA->MODER &= ~(GPIO_MODER_MODE15_Msk);
+			GPIOA->MODER |=  GPIO_MODER_MODE15_0;
+			GPIOA->BSRR  |=   GPIO_BSRR_BS_15;
+		}
+		else
+		{
+			GPIOA->BSRR  |=   GPIO_BSRR_BR_15;
+		}
+
+		meme++;
+	}
+
+	NVIC_ClearPendingIRQ(RTC_IRQn);
+}
+
+void sleeper(void)
+{
+	RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+		PWR->CR  &= ~PWR_CR_PDDS;
+		PWR->CSR &= ~PWR_CSR_WUF;
+
+		EXTI->PR = 0;
+
+		SCB->SCR |= 1 << SCB_SCR_SLEEPDEEP_Pos;
+
+		__WFE();
+}
+
 int main(void)
 {
-	PKM_Enable();
+	Real_time_clock_enable();
 
-	while(1)
+	RTC_Set_WakeUp_Timer();
+
+    while (1)
     {
-
+    	sleeper();
     }
 }
