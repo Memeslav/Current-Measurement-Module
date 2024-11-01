7e65ad062fcf9546b0d50e73cde550fa7b21bd6a
Memeslav <122861199+Memeslav@users.noreply.github.com>
2024-11-01 09:55:03 +0300

    Try to replace LSE with LSI...

--- Core/Src/Real_time_clock.c
+++ Core/Src/Real_time_clock.c
@@ -74,8 +74,6 @@ void Real_time_clock_enable(void)
 
     Watchdog_Enable(&RTC_Frequence);
 
-
-
     RCC->APB1ENR |=  RCC_APB1ENR_PWREN;
 
 		PWR->CR  |=  PWR_CR_DBP;
@@ -83,10 +81,7 @@ void Real_time_clock_enable(void)
 		RCC->CSR |=  RCC_CSR_RTCRST_Msk;
 		RCC->CSR &= ~RCC_CSR_RTCRST_Msk;
 
-		RCC->CSR |=	 RCC_CSR_LSEON;
-		while((RCC->CSR & RCC_CSR_LSERDY) != RCC_CSR_LSERDY){}
-
-		RCC->CSR |=  RCC_CSR_RTCSEL_0;
+		RCC->CSR |=  RCC_CSR_RTCSEL_LSI;
 		RCC->CSR |=  RCC_CSR_RTCEN;
 
 		PWR->CR  &= ~PWR_CR_DBP;
