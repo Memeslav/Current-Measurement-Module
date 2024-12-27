245f9e6c9b2af4261f5c78cf9888f37ad017a7ac
Memeslav <122861199+Memeslav@users.noreply.github.com>
2024-12-27 12:53:12 +0300

    Cool worked DMA code

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -3,4 +3,6 @@
 
 #include "stm32l053xx.h"
 
+#include "Module_USART.h"
+
 #endif
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,9 +1,16 @@
 #include "main.h"
 
+char test[] = "Test! \r\n";
+
 int main(void)
 {
+	Module_USART_Init(9600);
+
     while (1)
     {
+    	for(int i = 0; i < 100000; i++);
 
+    	Module_USART_Transmit((uint8_t*)test, sizeof(test));
+    	for(int i = 0; i < 100000; i++);
     }
 }
