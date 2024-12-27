a5f8ae7a568662e18c1558bba92affaf79c284d7
Memeslav <122861199+Memeslav@users.noreply.github.com>
2024-12-27 10:22:07 +0300

    Removed files

--- Core/Memory/Inc/Firmware_version.h
+++
@@ -1,8 +0,0 @@
-#ifndef FIRMWARE_VERSION_H_
-#define FIRMWARE_VERSION_H_
-
-#include "Register.h"
-
-typedef Register_16b_t  FM_version_t;
-
-#endif
--- Core/Memory/Inc/Unixtime.h
+++
@@ -1,8 +0,0 @@
-#ifndef UNIXTIME_H_
-#define UNIXTIME_H_
-
-#include "Register.h"
-
-typedef	Register_32b_t Unixtime_t;
-
-#endif
